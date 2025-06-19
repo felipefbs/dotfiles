59623a5dd3db53c5ce7079cac8933f697b461c61
Felipe Ferreira <felipeffbs3x@gmail.com>
2025-06-19 18:58:30 -0300

    adicionando mais programas na instalaćão e as ferramentas do zsh

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,13 @@
 sudo apt update && sudo apt upgrade
 
-sudo apt install -y git \
-  ripgrep
+sudo apt install -y \
+  git \
+  ripgrep \
+  tmux \
+  zsh \
+  fzf \
+  stow
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
